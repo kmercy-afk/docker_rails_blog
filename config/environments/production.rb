2afc4652d1c2bc7f1c342efdea3aa7fdfad0d678
Mercy Wanji <kmercyafk@gmail.com>
2026-06-11 17:36:43 +0300

    Complete Docker Rails blog app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.hosts.clear
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
